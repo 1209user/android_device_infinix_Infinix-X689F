396fb13cbc7b33efa020e0a48f95b09e933d4251
1209user <163229477+1209user@users.noreply.github.com>
2024-03-19 01:46:48 -0300

    vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X689F-user
-add_lunch_combo omni_Infinix-X689F-userdebug
-add_lunch_combo omni_Infinix-X689F-eng
+add_lunch_combo twrp_Infinix-X689F-user
+add_lunch_combo twrp_Infinix-X689F-userdebug
+add_lunch_combo twrp_Infinix-X689F-eng
